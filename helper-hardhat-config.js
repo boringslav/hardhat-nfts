e884f703dbec206f4f8e81d8fd2d412ddd813254
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-02 17:53:53 +0300

    🔐 Add Chainlink VRF subscriptionId

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -15,16 +15,7 @@ const networkConfig = {
         fundAmount: "1000000000000000000",
         keepersUpdateInterval: "30",
         ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
-    },
-    42: {
-        name: "kovan",
-        linkToken: "0xa36085F69e2889c224210F603D836748e7dC0088",
-        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
-        oracle: "0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8",
-        jobId: "d5270d1c311941d0b08bead21fea7747",
-        fee: "100000000000000000",
-        fundAmount: "100000000000000000", // 0.1
-        keepersUpdateInterval: "30",
+        subscriptionId: "17869",
     },
     4: {
         name: "rinkeby",
@@ -32,6 +23,7 @@ const networkConfig = {
         ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
         keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        subscriptionId: "17869",
         oracle: "0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8",
         jobId: "6b88e0402e5d415eb946e528b8e0c7ba",
         fee: "100000000000000000",
