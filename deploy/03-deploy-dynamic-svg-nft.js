c97a1da153d85954b926cabbb6a1ef94189e74ac
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-08 11:37:49 +0300

    ✨ Deploy Script for DynamicSvgNft contract

--- deploy/03-deploy-dynamic-svg-nft.js
+++ deploy/03-deploy-dynamic-svg-nft.js
@@ -1,6 +1,7 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const fs = require("fs")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -15,4 +16,23 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
     }
+
+    const lowSVG = await fs.readFileSync("./images/dynamic/frown.svg", { encoding: "utf8" })
+    const highSVG = await fs.readFileSync("./images/dynamic/happy.svg", { encoding: "utf8" })
+
+    log("-----------------")
+    const args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    log("---------------------------")
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(dynamicSvgNft.address, args)
+    }
 }
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
